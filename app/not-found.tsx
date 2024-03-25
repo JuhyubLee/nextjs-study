c93815122b29212c89b8ab5310a2621c3a61fed0
JuhyubLee <60839817+JuhyubLee@users.noreply.github.com>
2024-03-25 22:20:53 +0900

    Add files via upload

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+import Navigation from "../components/navigation";
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: "Not found"
+}
+export default function NotFound() {
+    return <div>
+        <h1>Not Found!</h1>
+    </div>
+}
