7a98cc28e671ae106d681f3a14618d2ee8772437
JH <scall24@naver.com>
2024-03-25 22:38:36 +0900

    MODFITY: API_URL 오류 수정

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,6 +1,5 @@
-import {API_URL} from "../app/(home)/page";
-
 async function getMovie(id: string) {
+    const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
     console.log(`Fetching movies : ${Date.now()}`)
     // await new Promise((resolve) => setTimeout(resolve, 3000));
     const response = await fetch(`${API_URL}/${id}`);
